b8ac6d0cf91713bc32b43acf964df7a91587ace7
Juh4nLucas <105506499+Juh4nLucas@users.noreply.github.com>
2022-05-14 18:24:10 -0400

    Update Projeto_LookBus.java

--- LookBus_Projeto/src/lookBus/Projeto_LookBus.java
+++ LookBus_Projeto/src/lookBus/Projeto_LookBus.java
@@ -4,8 +4,8 @@ public class Projeto_LookBus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Ol· Mundo!");
-    System.out.println("Ser· que foi?");
+    System.out.println("Ol√° Mundo!");
+    System.out.println("Alterado pelo Github");
 	}
 
 }
