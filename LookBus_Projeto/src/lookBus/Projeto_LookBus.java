cb83c67b4a3fd91c5d9c1446d0104574019e2664
Juh4nLucas <105506499+Juh4nLucas@users.noreply.github.com>
2022-05-14 17:51:25 -0400

    Adicionei arquivos via Upload

---
+++ LookBus_Projeto/src/lookBus/Projeto_LookBus.java
@@ -0,0 +1,10 @@
+package lookBus;
+
+public class Projeto_LookBus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Olá Mundo!");
+	}
+
+}
