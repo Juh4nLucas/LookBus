64e267a1a2d996ac18bc048aab5fd5294b583743
Juhan Lucas <Juhan Lucas@JuhanLucas>
2022-05-14 18:04:04 -0400

    Adicionei uma linha no código

--- LookBus_Projeto/src/lookBus/Projeto_LookBus.java
+++ LookBus_Projeto/src/lookBus/Projeto_LookBus.java
@@ -5,6 +5,7 @@ public class Projeto_LookBus {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("Olá Mundo!");
+    System.out.println("Será que foi?");
 	}
 
 }
